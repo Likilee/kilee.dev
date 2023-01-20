1458058f9b25cf955a857767b079bf9709dfc85e
Likilee <gnsdlrl@daum.net>
2023-01-21 02:23:49 +0900

    feat: write mdxToHtml utility function

--- lib/mdx.ts
+++ lib/mdx.ts
@@ -1,9 +1,23 @@
 import { readFileSync } from "fs"
 import { POST_DIR } from "lib/files"
+import { bundleMDX } from "mdx-bundler";
 import path from "path"
+import rehypeHighlight from "rehype-highlight";
 
 export const getMDXSourceFromLocal = (fileName: string) => {
   const filePath = path.join(POST_DIR, fileName);
   const mdxSource = readFileSync(filePath, 'utf-8');
   return mdxSource;
+}
+
+type MDXtoHtmlFn = {
+  (mdxSource: string): Promise<{code: string}>
+}
+
+export const mdxToHtml: MDXtoHtmlFn = async (mdxSource: string) => {
+  const {code, frontmatter} = await bundleMDX({source: mdxSource, cwd: process.cwd(), mdxOptions(options, frontmatter) {
+    options.rehypePlugins = [...(options.rehypePlugins ?? []), rehypeHighlight];
+    return options;
+  }})
+  return { code }
 }
