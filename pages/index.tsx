25ccb1a9951dbe454994da96baf139eb07293afa
Likilee <gnsdlrl@daum.net>
2023-01-21 22:34:36 +0900

    refactor: home page content

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,9 +5,19 @@ import { createClient } from 'next-sanity'
 export default function Home({ posts }: InferGetServerSidePropsType<typeof getStaticProps>) {
   return (
     <PageLayout>
-      {posts.map((post, index) => (
-        <div key={index}>${post.content}</div>
-      ))}
+      <div>
+      <h1 className="mb-1 text-3xl font-bold tracking-tight  md:text-5xl">
+        Kilee
+      </h1>
+      <h2 className="mb-4">
+        Frontend Developer
+      </h2>
+      <p className="text-gray-500 dark:text-gray-400">
+        좋은 습관을 많이 가진
+        괜찮은 개발자가 되고자해요.
+        농구 스포츠 클라이밍 웨이크보드를 좋아해요.
+      </p>
+      </div>
     </PageLayout>
   )
 }
