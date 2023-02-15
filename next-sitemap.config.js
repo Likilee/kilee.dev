f913d5c5d6165e49141ddda3cf40caaa407eeb2f
Likilee <gnsdlrl@daum.net>
2023-02-15 22:31:09 +0900

    fix:

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,6 @@
 /* 💡 https://www.npmjs.com/package/next-sitemap */
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_VERCEL_URL
-    ? 'https://' + process.env.NEXT_PUBLIC_VERCEL_URL
-    : 'https://kilee.dev',
+  siteUrl: process.env.SITE_URL || 'https://kilee.dev',
   generateRobotsTxt: true, // (optional)
   sitemapSize: 7000,
   // ...other options
