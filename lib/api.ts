0a2699e24c2d0b3f94cb7b654863d12dc19ca9b6
Likilee <gnsdlrl@daum.net>
2023-02-18 18:07:27 +0900

    fix: api route

--- lib/api.ts
+++ lib/api.ts
@@ -1,18 +1,13 @@
 import { ViewsTable } from 'lib/planetscale'
 
-const apiServer =
-  process.env.NODE_ENV === 'production'
-    ? `${process.env.SITE_URL}/api`
-    : 'http://localhost:3000/api'
-
 /* 💡 Get All View counts */
 export const getAllView = async (): Promise<ViewsTable[]> => {
-  const res = await fetch(`${apiServer}/views`)
+  const res = await fetch(`/views`)
   return res.json()
 }
 
 /* 💡 Post View Count with slug */
 export const postViewBySlug = async (slug: string) =>
-  fetch(`${apiServer}/views/${slug}`, {
+  fetch(`/views/${slug}`, {
     method: 'POST',
   })
