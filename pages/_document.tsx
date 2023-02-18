595f63b3085e2eb33b0c5a8048ed618e3da34d62
Likilee <gnsdlrl@daum.net>
2023-02-19 01:23:04 +0900

    refactor: add ko to html lang

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="ko">
       <Head />
 
       <body className="bg-gray-50 text-black dark:bg-gray-900 dark:text-white">
