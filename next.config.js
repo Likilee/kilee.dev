1748bfa6a05508ee65f07fa95245bb5bab5e3ce9
Likilee <gnsdlrl@daum.net>
2023-07-19 02:58:41 +0900

    fix: Application Error
    swcMinify have problem with korean

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const { withContentlayer } = require('next-contentlayer')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: false,
   webpack: (config) => {
     config.experiments.topLevelAwait = true
     return config
