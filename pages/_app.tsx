b1be774ed2bfb6f27010b3db11dae39d596a7dce
Likilee <gnsdlrl@daum.net>
2023-07-27 21:03:56 +0900

    ci: add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from 'next-themes'
 import BaseLayout from 'layouts/BaseLayout'
 import localFont from '@next/font/local'
 import cn from 'classnames'
-
+import { Analytics } from '@vercel/analytics/react'
 const pretendardVaribale = localFont({
   src: '../public/font/PretendardKR-VF-distilled.woff2',
   variable: '--font-pretendard',
@@ -26,16 +26,19 @@ const queryClient = new QueryClient({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class">
-      <QueryClientProvider client={queryClient}>
-        <Hydrate state={pageProps.dehydratedState}>
-          <main className={cn(pretendardVaribale.variable, 'font-sans')}>
-            <BaseLayout>
-              <Component {...pageProps} />
-            </BaseLayout>
-          </main>
-        </Hydrate>
-      </QueryClientProvider>
-    </ThemeProvider>
+    <>
+      <ThemeProvider attribute="class">
+        <QueryClientProvider client={queryClient}>
+          <Hydrate state={pageProps.dehydratedState}>
+            <main className={cn(pretendardVaribale.variable, 'font-sans')}>
+              <BaseLayout>
+                <Component {...pageProps} />
+              </BaseLayout>
+            </main>
+          </Hydrate>
+        </QueryClientProvider>
+      </ThemeProvider>
+      <Analytics />
+    </>
   )
 }
